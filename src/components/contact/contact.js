33784c8543112f859eed0e8678c2dc2d5484c91b
Emmanuel Lerma <Emmanuellerma96@gmail.com>
2022-09-19 23:05:28 -0500

    overlay text over dot

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,17 +5,39 @@ export const Contact = () => {
     return (
         <div className='contact-container'>
             <div>
-                <h2>Contact</h2>
+                <div className='title'>
+                    <span className="dot"></span>
+                    <h2>Contact</h2>
+                </div>
                 <h4>Want to get a hold of me?</h4>
                 <br></br>
                 <h4>Just want to say hi? Drop a message here!</h4>
             </div>
             <br></br>
-            <div className='input-container'>
+            <div className='contact-links-container'>
+                <div className='link-row'>
+                    <img src=''/>
+                    <h3>Emmanuellerma@outlook.com</h3>
+                </div>
+                <div className='link-row'>
+                    <img src=''/>
+                    <h3>Github Link</h3>
+                </div>
+                <div className='link-row'>
+                    <img src=''/>
+                    <h3>Linkedin Link</h3>
+                </div>
+                <div className='link-row'>
+                    <img src=''/>
+                    <h3>View My Resume</h3>
+                </div>
+
+            </div>
+            {/* <div className='input-container'>
                 <input placeholder='Your Name'></input>
                 <input placeholder='Your Email'></input>
                 <input type='text' rows={5} placeholder='Your Message'></input>
-            </div>
+            </div> */}
         </div>
     )
 }
